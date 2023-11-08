5e70cb04fd303c1ac0657abf4d4537a9208d8717
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2023-11-08 17:19:17 -0500

    Update backend (#3)

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -26,10 +26,10 @@ urlpatterns = [
     path(route='logout/', view=views.logout_request, name='logout'),
 
     # path for dealer reviews view
-    #path(route='dealer/<int:dealer_id>/', view=views.get_dealer_details, name='dealer_details'),
+    path('dealer/<int:dealer_id>/<str:dealer_name>/', views.get_dealer_details, name='dealer_details'),
 
     # path for add a review view
-    #path(route='dealer/<int:dealer_id>/', view=views.add_review, name='add_review'),
+    path('dealer/<int:dealer_id>/<str:dealer_name>/addreview/', views.add_review, name='add_review')
 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
